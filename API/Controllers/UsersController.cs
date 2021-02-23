103117d9a05f71080ec566f1fd912cda7a5a8dda
harsh1930 <hchouhan@unomaha.edu>
2021-02-23 01:49:05 -0600

    JWT token implementation, added some extension methods to clean up the code.

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -21,15 +21,15 @@ namespace API.Controllers
         }   
 
         [HttpGet]
-       // [AllowAnonymous]
+       [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
 
             return await _context.Users.ToListAsync();
         }
-       //[Authorize]
+       [Authorize]
         [HttpGet("{id}")]
-        public  async Task<ActionResult<AppUser>> GetUsers(int id)
+        public  async Task<ActionResult<AppUser>> GetUser(int id)
         {
             return await _context.Users.FindAsync(id);
         } 
