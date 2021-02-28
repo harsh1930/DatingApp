a93d84b7c8ee1feb8941e858434b5971f7a49e5a
harsh1930 <hchouhan@unomaha.edu>
2021-02-28 04:30:36 -0600

    Added home componet, register component and nav component. Register and login functionality is working.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 
 @Component({
@@ -10,19 +12,16 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit{
   title = 'The Dating App';
   users: any;
-  constructor(private http: HttpClient){
+  constructor( private accountService : AccountService){
   }
 
   ngOnInit() {
-   this.getUsers();
+   this.setcurrentUser();
   }
  
-  getUsers(){
-    
-    this.http.get('https://localhost:5001/api/users').subscribe(respons=> {
-      this.users = respons;
-    }, error=> {
-      console.log(error);
-    })
+  setcurrentUser(){
+const user : User = JSON.parse(localStorage.getItem('user')) ;
+this.accountService.setCurrentUser(user);
   }
+ 
 }
